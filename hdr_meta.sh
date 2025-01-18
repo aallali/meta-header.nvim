ab6f77ccb088266dcd5f41387723ae51ca5f0e61
3dida <44850191+aallali@users.noreply.github.com>
2025-01-18 04:12:52 +0100

    feat: Add auto generated env sertter for testing

---
+++ hdr_meta.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Fill in the following environment variables
+export HDR_AUTHOR="Copyright © 2025 <hi@allali.me>"
+export HDR_USERNAME="aallali"
+export HDR_PROJECT="meta-header.nvim"
+export HDR_LICENSE="MIT"
+
+echo "AUTHOR: $HDR_AUTHOR"
+echo "USERNAME: $HDR_USERNAME"
+echo "PROJECT: $HDR_PROJECT"
+echo "LICENSE: $HDR_LICENSE"
