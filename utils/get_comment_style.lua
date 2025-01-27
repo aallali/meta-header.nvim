c0ec91955bd35b0108a114f28102921f14623d82
aallali <contact@allali.me>
2025-01-28 00:48:44 +0100

    feat: add comment style for javascript files

--- utils/get_comment_style.lua
+++ utils/get_comment_style.lua
@@ -6,7 +6,7 @@
 --   License : MIT                                                            --
 --                                                                            --
 --   Created: 2025/01/18 02:23:11 by aallali                                  --
---   Updated: 2025/01/24 21:50:00 by aallali                                  --
+--   Updated: 2025/01/27 00:52:32 by aallali                                  --
 -- ************************************************************************** --
 -- Determine the comment style based on the file type
 local function get_comment_style()
@@ -19,7 +19,7 @@ local function get_comment_style()
         return "\"", "\""
     elseif extension == "lua" or filetype == "lua" then
         return "--", "--"
-    elseif extension == "c" or extension == "h" or extension == "cpp" or extension == "hpp" then
+    elseif extension == "js" or extension == "ts" or extension == "c" or extension == "h" or extension == "cpp" or extension == "hpp" then
         return "/*", "*/"
     elseif extension == "html" or extension == "xml" then
         return "<!--", "-->"
