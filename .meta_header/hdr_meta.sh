a5ce68b3da69c3aea98468ccd6240db89fda7766
aallali <contact@allali.me>
2025-02-08 01:45:10 +0100

    feat: upload  generated `.meta_header`

--- .meta_header/hdr_meta.sh
+++ .meta_header/hdr_meta.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# Plugin: https://github.com/aallali/meta_header.nvim/
 # Fill in the following environment variables
 export HDR_AUTHOR="Copyright © 2025 <hi@allali.me>"
 export HDR_USERNAME="aallali"
-export HDR_PROJECT="meta-header.nvim"
+export HDR_PROJECT="meta_header.nvim"
 export HDR_LICENSE="MIT"
 
+# source this file after modify (source hdr_meta.sh)
 echo "AUTHOR: $HDR_AUTHOR"
 echo "USERNAME: $HDR_USERNAME"
 echo "PROJECT: $HDR_PROJECT"
