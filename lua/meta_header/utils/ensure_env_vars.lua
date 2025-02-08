df59e072b3cc4b0afcf2ac883a4c4e475bcb46fe
aallali <contact@allali.me>
2025-02-08 01:44:13 +0100

    feat: create `.meta_header` folder in root folder for configs

--- lua/meta_header/utils/ensure_env_vars.lua
+++ lua/meta_header/utils/ensure_env_vars.lua
@@ -6,23 +6,29 @@
 --   License : MIT                                                            --
 --                                                                            --
 --   Created: 2025/01/18 02:19:33 by aallali                                  --
---   Updated: 2025/01/28 14:33:15 by aallali                                  --
+--   Updated: 2025/02/07 22:01:08 by aallali                                  --
 -- ************************************************************************** --
 -- Function to check and set HDR environment variables
 local function ensure_env_vars()
     -- Get the directory of the current file
-    local current_file_path = vim.fn.expand('%:p:h')
-    local hdr_meta_path = current_file_path .. "/hdr_meta.sh"
+    local current_dir_path = vim.loop.cwd()
+    local meta_header_dir = current_dir_path .. "/.meta_header"
+    local hdr_meta_path = meta_header_dir .. "/hdr_meta.sh"
 
     if not os.getenv("HDR_AUTHOR") or not os.getenv("HDR_USERNAME") or not os.getenv("HDR_PROJECT") or
         not os.getenv("HDR_LICENSE") then
+        -- Create .meta_header directory if it doesn't exist
+        if vim.fn.isdirectory(meta_header_dir) == 0 then
+            vim.fn.mkdir(meta_header_dir, "p")
+        end
+
         if vim.fn.filereadable(hdr_meta_path) == 0 then
-            -- Create the template file in the current directory
+            -- Create the template file in the .meta_header directory
             local file = io.open(hdr_meta_path, "w")
             file:write([[
 #!/bin/bash
 
-# Plugin: https://github.com/aallali/meta-header.nvim/
+# Plugin: https://github.com/aallali/meta_header.nvim/
 # Fill in the following environment variables
 export HDR_AUTHOR="<Copyright © Your Name Here>"
 export HDR_USERNAME="<Your Username Here>"
