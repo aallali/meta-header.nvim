16d65c7cdd41c41c76b7ded6b4669fe39ca07d3d
aallali <contact@allali.me>
2025-02-09 01:50:07 +0100

    fix: handle created at dates on different templates
    handle these cases in regex:
    - `Created:`
    - `Created`
    - `Created :`
    - `Created : `

--- lua/meta_header/utils/extract_existing_metadata.lua
+++ lua/meta_header/utils/extract_existing_metadata.lua
@@ -1,15 +1,29 @@
+-- ************************************************************ --
+--    Copyright © 2025 <hi@allali.me>                ┏┓ ┏┓ ┏┓   --
+--                                                   ┃┫ ┏┛ ┃┫   --
+--    File    : extract_existing_metadata.lua        ┗┛•┗━•┗┛   --
+--    Project : meta_header.nvim                                --
+--    License : MIT                                             --
+--                                                              --
+--    Created: 2025/01/18 02:48:33 by aallali                   --
+--    Updated: 2025/02/09 01:20:58 by aallali                   --
+-- ************************************************************ --
+
 local function extract_existing_metadata(lines)
     local existing_created_at, existing_created_by
     local modified_lines = vim.deepcopy(lines)
 
     for i, line in ipairs(modified_lines) do
-        existing_created_at = line:match("%s%s%sCreated:%s*(.-)%s*by")
+        -- Updated pattern to match "Created", "Created:" or "Created :"
+        existing_created_at = line:match("%s%s%sCreated%s*:?%s*(.-)%s*by")
         existing_created_by = line:match("by%s*(.-)%s*$")
 
         if existing_created_at and existing_created_by then
             -- Update the line with existing data
-            modified_lines[i] = line:gsub("%s%s%s(Created:%s*)(.-)(%s*by%s*)(.*)",
-                "%1" .. existing_created_at .. "%3" .. existing_created_by)
+            modified_lines[i] = line:gsub(
+                "%s%s%s(Created%s*:?%s*)(.-)(%s*by%s*)(.*)",
+                "%1" .. existing_created_at .. "%3" .. existing_created_by
+            )
             break
         end
     end
